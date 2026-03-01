3c900a446b205b3bebec0fa4e2a64940ea6cb0ba
Marc Crenshaw <dubeune@gmail.com>
2026-03-01 15:16:09 -0600

    fixed vite config and ran npm run build and npm run dev.  Still trying to deploy.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true, // This ensures Blade/Livewire files trigger a reload
+            refresh: true,
         }),
         tailwindcss(),
     ],
